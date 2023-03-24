aa73d20d980755abc9682b2704bff0b542c0c196
Christopher-Deo <chris@christopherdeo.com>
2023-03-23 20:58:23 -0500

    finalized script files

---
+++ main.py
@@ -0,0 +1,14 @@
+#input to get user supplied year to be checked
+year = int(input("Which year do you want to check? "))
+
+#logic to determine if the year entered is a leap year
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print('Leap Year!')
+        else:
+            print('Not a leap year.')
+    else:
+        print('Leap Year!')
+else:
+    print('Not a leap year.')
